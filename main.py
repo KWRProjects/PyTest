98ca3db758304006ae89568612e80b41921f426b
quanpan302 <quanpan302@hotmail.com>
2021-02-22 00:24:46 +0100

    Test GDAL

--- main.py
+++ main.py
@@ -7,6 +7,84 @@
 """
 
 
+def gdal():
+    import sys
+    from osgeo import gdal
+    from osgeo import ogr
+
+    def raw_input():
+        return "1 1 a"
+
+    driverName = "ESRI Shapefile"
+    drv = gdal.GetDriverByName(driverName)
+    if drv is None:
+        print("%s driver not available.\n" % driverName)
+        sys.exit(1)
+
+    ds = drv.Create("Output/test_point_out.shp", 0, 0, 0, gdal.GDT_Unknown)
+    if ds is None:
+        print("Creation of output file failed.\n")
+        sys.exit(1)
+
+    lyr = ds.CreateLayer("point_out", None, ogr.wkbPoint)
+    if lyr is None:
+        print("Layer creation failed.\n")
+        sys.exit(1)
+
+    field_defn = ogr.FieldDefn("Name", ogr.OFTString)
+    field_defn.SetWidth(32)
+
+    if lyr.CreateField(field_defn) != 0:
+        print("Creating Name field failed.\n")
+        sys.exit(1)
+
+    # Expected format of user input: x y name
+    linestring = raw_input()
+    linelist = linestring.split()
+
+    x = float(linelist[0])
+    y = float(linelist[1])
+    name = linelist[2]
+
+    feat = ogr.Feature(lyr.GetLayerDefn())
+    feat.SetField("Name", name)
+
+    pt = ogr.Geometry(ogr.wkbPoint)
+    pt.SetPoint_2D(0, x, y)
+
+    feat.SetGeometry(pt)
+
+    if lyr.CreateFeature(feat) != 0:
+        print("Failed to create feature in shapefile.\n")
+        sys.exit(1)
+
+    feat.Destroy()
+
+    # while len(linelist) == 3:
+    #     x = float(linelist[0])
+    #     y = float(linelist[1])
+    #     name = linelist[2]
+    #
+    #     feat = ogr.Feature(lyr.GetLayerDefn())
+    #     feat.SetField("Name", name)
+    #
+    #     pt = ogr.Geometry(ogr.wkbPoint)
+    #     pt.SetPoint_2D(0, x, y)
+    #
+    #     feat.SetGeometry(pt)
+    #
+    #     if lyr.CreateFeature(feat) != 0:
+    #         print("Failed to create feature in shapefile.\n")
+    #         sys.exit(1)
+    #
+    #     feat.Destroy()
+    #
+    #     linestring = raw_input()
+    #     linelist = linestring.split()
+
+    ds = None
+
+
 def wntr():
     """
     The following example demonstrates how to import WNTR, generate a water network
@@ -36,7 +114,8 @@ def main():
     # Code goes over here.
     a = "a"
 
-    wntr()
+    # wntr()
+    gdal()
 
     return 0
 
